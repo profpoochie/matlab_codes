fc298492d4f0e5e7d93ffba5ac57accf8c305646
profpoochie <homepoochie@gmail.com>
2020-03-11 22:51:09 +0800

    changed gr to C0

--- Design of Functional Generator/triangular_code.m
+++ Design of Functional Generator/triangular_code.m
@@ -7,14 +7,15 @@ clc;        % clear command window
 t = linspace(0,0.3,10000);  % definition of t form 0 to 4*pi
 f = 10;
 w = 2*pi*f;
-gr = 1/2;
-gr100 = triagular(gr,w,t,10000);   %assume with perfect precision
+C0 = 1/2;
+gr100 = triagular(C0,w,t,10000);   %assume with perfect precision
 grid on;
 
 
 % function for Fourier Series computation
 
-function gr = triagular(gr,w,t,last)
+function gr = triagular(C0,w,t,last)
+    gr=C0;
     for i=1:last
         C = (2/(i*pi)^2)*(cos(i*pi)-1);
         gr = gr+C*cos(w*i.*t);
